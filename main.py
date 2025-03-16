e30b670219b63b339e4ce69461cb38feceb396eb
Equat_ion <areebahmadsyed09@gmail.com>
2025-03-16 11:45:59 +0530

    added base api functionality with hardcoded messgaes

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+import json
+import random
+
+app = FastAPI()
+
+EXPENSE_FILE = 'expenses.json'
+
+try:
+    with open(EXPENSE_FILE, 'r') as f:
+        expenses = json.load(f)
+
+except FileNotFoundError:
+    expenses = {"total": 0, 'logs': []}
+
+MATA_REACTION = {
+    "good": ["Good beta, keep saving!", "Proud of you, but buy some dhaniya too."],
+    "warning": ["Hmm… not too much spending, okay?", "Do you think money grows on trees?"],
+    "bad": ["AREY! ₹8000 ka pizza? CHAPPAL KHAEGA?", "Should I call your real mom?", "Bas kar beta, bas kar."]
+}
+
+
+@app.get("/")
+async def root():
+    return {'message':'Mummy budget tracker is running.'}
+
+@app.post("/add_expense/")
+async def add_expense(amount:int, description:str='Unknown'):
+    
+    global expenses
+    expenses['total'] += amount
+    expenses['logs'].append({'amount':amount, 'description':description})
+
+    with open(EXPENSE_FILE,'w') as f:
+        json.dump(expenses,f)
+
+    if amount < 500:
+        response = random.choice(MATA_REACTION["good"])
+    elif amount < 5000:
+        response = random.choice(MATA_REACTION["warning"])
+    else:
+        response = random.choice(MATA_REACTION["bad"])
+
+    return {"message": response, "total_spent": expenses["total"]}
