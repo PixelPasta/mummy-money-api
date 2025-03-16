8336e546d1374867ff43ef0589e404f4c3b6461d
Equat_ion <areebahmadsyed09@gmail.com>
2025-03-16 14:00:26 +0530

    added support for gemini based ai responses

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
 import json
-import random
 import google.generativeai as genai
 
 
 genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel(model_name='gemini-2.0-flash')
+
+def get_mummy_response(amount:int, description:str) -> str:
+    if amount < 0:
+        raise ValueError('Amount must be valid')
+
+    prompt = f"""
+    You are an angry indian middle-class mother. Your job is to react to child's expenses,
+    if they spend too much, you should scold them and in a funny response.
+    if they save money, you should praise them but still nag them a little.
+    you should also consider the reason and price before giving you response.
+
+    The Child spent {amount} for the purpose of {description}.
+
+    React like an Angry middle-aged middle-class mother and you responses should all be in
+    hinglish and concise in less than 10 words
+    """
+
+    response = model.generate_content(prompt)
+    return response.text.strip() if response and response.text else 'Beta, kya kar raha hai?!'
 
 
 
@@ -19,11 +38,6 @@ try:
 except FileNotFoundError:
     expenses = {"total": 0, 'logs': []}
 
-MATA_REACTION = {
-    "good": ["Good beta, keep saving!", "Proud of you, but buy some dhaniya too."],
-    "warning": ["Hmm… not too much spending, okay?", "Do you think money grows on trees?"],
-    "bad": ["AREY! ₹8000 ka pizza? CHAPPAL KHAEGA?", "Should I call your real mom?", "Bas kar beta, bas kar."]
-}
 
 
 @app.get("/")
@@ -44,12 +58,8 @@ async def add_expense(amount:int, description:str='Unknown'):
     with open(EXPENSE_FILE,'w') as f:
         json.dump(expenses,f)
 
-    if amount < 500:
-        response = random.choice(MATA_REACTION["good"])
-    elif amount < 5000:
-        response = random.choice(MATA_REACTION["warning"])
-    else:
-        response = random.choice(MATA_REACTION["bad"])
+
+        response = get_mummy_response(amount, description)
 
     return {"message": response, "total_spent": expenses["total"]}
 
